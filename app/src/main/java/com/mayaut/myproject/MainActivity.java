37dba3ace2a21ce4aab5d3e9556c2685bddb7019
mayaut <utmaya@gmail.com>
2026-04-13 01:30:24 -0400

    Refactor MainActivity to use fragments for navigation and update layout files. Removed deprecated home fragment and added new fragments for home, alarms, and flashcards. Updated string resources and themes for consistency.

--- app/src/main/java/com/mayaut/myproject/MainActivity.java
+++ app/src/main/java/com/mayaut/myproject/MainActivity.java
@@ -2,11 +2,7 @@ package com.mayaut.myproject;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,10 +16,29 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        Fragment homeFragment = new home();
+        Fragment homeFragment = new HomeFragment();
+        Fragment alarmFragment = new AlarmFragment();
+        Fragment flashcardsFragment = new FlashcardsFragment();
 
+        setCurrentFragment(homeFragment);
 
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                setCurrentFragment(homeFragment);
+            } else if (itemId == R.id.nav_alarm) {
+                setCurrentFragment(alarmFragment);
+            } else if (itemId == R.id.nav_flashcards) {
+                setCurrentFragment(flashcardsFragment);
+            }
+            return true;
+        });
+    }
 
-
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
     }
 }
