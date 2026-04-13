68711dd25d750a560a55510b48d470c9bdbe4646
mayaut <utmaya@gmail.com>
2026-04-13 02:17:32 -0400

    commit4

--- app/src/main/java/com/mayaut/myproject/HomeFragment.java
+++ app/src/main/java/com/mayaut/myproject/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.mayaut.myproject;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,5 +17,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        boolean alarmStarted;
+//        if (!alarmStarted) {
+//            alarmStarted = true;
+//
+//            new Handler().postDelayed(() -> {
+//                NavController navController = Navigation.findNavController(view);
+//                navController.navigate(R.id.alarmFragment);
+//            }, 5000); // 5 seconds
+//        }
     }
 }
